0bc8128ee8ef582c28341d4291a15e23311a9d6d
R4BB1T <44379716+M-R4BB1T@users.noreply.github.com>
2019-02-23 15:10:13 -0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,16 +1,7 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
-const Twit = require('twit');
 
-var T = new Twit({
-  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-  access_token:         process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms:           60*1000,
-  strictSSL:            true,     
-})
 
 module.exports = async (client, member) => {
 
