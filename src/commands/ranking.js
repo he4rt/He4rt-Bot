ebeedfa27e3a708332d230f138acc993db30774a
Gustavo M <gugumllhlimaa@gmail.com>
2019-03-09 13:18:33 -0300

    Ranking command

--- src/commands/ranking.js
+++ src/commands/ranking.js
@@ -2,38 +2,35 @@ const Discord = require('discord.js');
 
 module.exports = {
    run:  (client, message, args) => {
+
+    if(args[0] < 1 || args[0].includes(",") || args[0].includes("-") || args[0].includes("*") || isNaN(args[0])) return message.channel.send("Tem q ser maior que 0");
+    
     const page = args[0] ? args[0] : 1
     let count = args[0] ? args[0] : 0
     count = count * 10
-    console.log(count)
     client.axios.get('/ranking?page='+ page)
     .then(res => {
-        console.log(res.data)
+      
+      if(res.data.data[0] == null) return message.channel.send("Essa pagina nao existe");
+      
         const ranking = res.data
         const value = []
         for(let i in ranking.data){
             let rank = ranking.data[i]
             let user =  client.guilds
-            .get(process.env.GUILD_ID)
+            .get("452926217558163456")
             .members.get(ranking.data[i].discord_id).user
             value.push( (1 + parseInt(i) + (count - 10)  ) + " | "+ user.username + " | Level: " + rank.level  + " | Exp: " + rank.current_exp)
         }
 
 
         const embed = new Discord.RichEmbed()
-        .setTitle("Rankingzera")
-        .setAuthor(client.user.username, client.user.avatarURL)
-        .setColor(0x00AE86)
-        .setDescription("This is the main body of text, it can hold 2048 characters.")
+        .setTitle('``🏆`` » !ranking')
+        .addField("Pagina #"+ page + " de " + ranking.last_page, value, true)
         .setTimestamp()
-        .setFooter(
-            '2019 © He4rt Developers',
-            'https://heartdevs.com/wp-content/uploads/2018/12/logo.png'
-        )
-
-        .addField("Pagina "+ page + " de " + ranking.lastPage, value, true)
         message.channel.send({embed});
-    }).catch(res => console.log("deu merda no ranking"))
+    }).catch(error => console.log(error))
+
     //message.delete().catch(() => {});
 
   },
