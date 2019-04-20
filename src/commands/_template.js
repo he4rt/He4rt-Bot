0c4293f2173c19f1fa6165bc1b93aa9f5bf3de00
Gustavo M <gugumllhlimaa@gmail.com>
2019-04-20 19:19:48 -0300

    Adicionando template

---
+++ src/commands/_template.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  async run(client, message) {
+    
+  },
+
+  get command() {
+    return {
+      name: 'nome do comando',
+      category: 'Admin, mod etc..',
+      description: 'Descrição do Comando',
+      usage: 'comando',
+    };
+  },
+};
