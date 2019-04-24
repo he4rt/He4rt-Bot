b67b0bd5f957316294674db14bfa9a439c970904
Gustavo M <gugumllhlimaa@gmail.com>
2019-04-24 20:19:47 -0300

    Alteração rep

---
+++ src/commands/rep.js
@@ -0,0 +1,36 @@
+
+module.exports = {
+  async run(client, message) {
+    const member = message.mentions.users.first();
+
+    if(!member) {
+        return message.channel.send('❌ | Utilize ``!rep <@usuário>``.')
+    }
+    
+    await client.axios.post(
+        `/users/${message.author.id}/reputation?receive_id=${member.id}`
+    ).then(res => {
+        return message.channel.send('✅ | Seu ponto de reputação foi entregue para ``' + member.username + '``!').then(msg => msg.delete(8000));
+    }).catch( error => {
+        if(error.response.status == '422') {
+          if(error.response.data.receive_id) {
+            return message.channel.send('❌ | Você não pode dar reputação para sí mesmo.').then(msg => msg.delete(8000));
+          } 
+          if(error.response.data.error_code == 'already.used.today') {
+            return message.channel.send('❌ | Você já utilizou seu ponto de reputação diário, aguarde **' + error.response.data.real_time + '**.').then(msg => msg.delete(8000));
+          } else {
+            return message.channel.send('❌ | Erro inesperado, tente novamente.').then(msg => msg.delete(8000));
+          }
+        }
+    })
+  },
+
+  get command() {
+    return {
+      name: 'rep',
+      category: 'Users',
+      description: 'Enviar reputações',
+      usage: 'rep <@usuário>',
+    };
+  },
+};
