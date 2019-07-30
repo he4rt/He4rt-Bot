9de6f25386ac68b3930e04cafd52b572a1d73e2f
JVictorV <40485924+JVictorV@users.noreply.github.com>
2019-07-30 00:40:28 -0300

    Log Function

---
+++ src/util/log.js
@@ -0,0 +1,29 @@
+const { bold } = require('chalk');
+
+const logTypes = {
+	WARN: {
+		color: bold.yellowBright,
+		content: 'WARN ',
+	},
+	ERROR: {
+		color: bold.redBright,
+		content: 'ERROR',
+	},
+	DEBUG: {
+		color: bold.greenBright,
+		content: 'DEBUG',
+	},
+};
+
+const log = (message, { color, content } = logTypes.DEBUG) => {
+	const tag = color(`[${content}]:`);
+	const clock = color(new Date().toLocaleTimeString());
+	const msg = color(` | ${message.toString() || message}`);
+
+	console.log(`${tag} ${clock} ${msg}`);
+};
+
+module.exports = {
+	log,
+	logTypes,
+};
