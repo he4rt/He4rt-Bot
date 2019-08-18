f12928da5444c76cce3b1f3310933be22f55a1ea
JVictorV <40485924+JVictorV@users.noreply.github.com>
2019-08-18 16:43:20 -0300

    Added Emblems Images support

--- src/util/jimpLoader.js
+++ src/util/jimpLoader.js
@@ -4,16 +4,18 @@ const { log, logTypes } = require('./log');
 
 const jimpLoader = async () => {
 	try {
-		const defaultImagesDir = await readdir('src/assets/images');
+		const imagesDir = await readdir('src/assets/images');
 		const fontsDir = (await readdir('src/assets/fonts')).filter(x => !x.includes('.png'));
 		const langsDir = await readdir('src/assets/langs');
+		const emblemsDir = await readdir('src/assets/emblems');
 
-		const assetsSize = defaultImagesDir.length + fontsDir.length + langsDir.length;
+		const assetsSize = imagesDir.length + fontsDir.length + langsDir.length + emblemsDir.length;
 		log(`Found ${assetsSize} assets`);
 
-		const imageLocations = defaultImagesDir.map(x => require.resolve(`../assets/images/${x}`));
+		const imageLocations = imagesDir.map(x => require.resolve(`../assets/images/${x}`));
 		const fontLocations = fontsDir.map(x => require.resolve(`../assets/fonts/${x}`));
 		const langImagesLocations = langsDir.map(x => require.resolve(`../assets/langs/${x}`));
+		const emblemImagesLocations = emblemsDir.map(x => require.resolve(`../assets/emblems/${x}`));
 
 		const images = await Promise.all(
 			imageLocations.map(async x => ({
@@ -41,10 +43,22 @@ const jimpLoader = async () => {
 			}),
 		);
 
-		log(`Loaded ${images.length + langImages.length} Images`);
+		const emblemImages = await Promise.all(
+			emblemImagesLocations.map(async x => {
+				const name = x.substring(x.lastIndexOf('\\') + 1, x.length);
+				const idName = `${name.substring(0, name.length - 4).toUpperCase()}_ROLE`;
+				return {
+					name,
+					res: await jimp.read(x),
+					id: process.env[idName],
+				};
+			}),
+		);
+
+		log(`Loaded ${images.length + langImages.length + emblemImages.length} Images`);
 		log(`Loaded ${fonts.length} Fonts`);
 
-		return { images, fonts, langImages };
+		return { images, fonts, langImages, emblemImages };
 	} catch (err) {
 		log(`Error when loading assets ${err.toString()}`, logTypes.ERROR);
 	}
