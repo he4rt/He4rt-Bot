4a01d546d676edac13b17d59294ebc705d602025
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-02-14 23:46:25 -0300

    feat(Commands): add !descrever <comando>

---
+++ src/Commands/Descrever.ts
@@ -0,0 +1,26 @@
+import Command from "@core/Contracts/Command"
+import Context from "@core/Contracts/Context"
+import InvalidArgsException from "@core/Exceptions/InvalidArgs"
+import Ioc from "@core/IoC/Ioc"
+
+export default class Descrever extends Command {
+  public get description() {
+    return "Mostra a descrição de um comando."
+  }
+
+  public help(): string {
+    return ":x:Como usar: `!descrever <comando>`"
+  }
+
+  public validate(args: string[]): void | never {
+    if (args.length === 0) {
+      throw new InvalidArgsException(this.help())
+    }
+  }
+
+  public async run({ args, send }: Context): Promise<void> {
+    const command = Ioc.use<Command>(args[0])
+
+    await send(command.description)
+  }
+}
