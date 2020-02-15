54b4b28d04318ab161f966cd9b97a6fdd5766e3b
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-02-14 22:54:47 -0300

    feat: add utility functions to MessageTransform return value

--- src/Core/Transformers/Message.ts
+++ src/Core/Transformers/Message.ts
@@ -7,7 +7,8 @@ import {
   PermissionOverwriteOptions,
   GuildChannel,
   RoleData,
-  Role
+  Role,
+  ChannelLogsQueryOptions
 } from "discord.js"
 
 import Context from "@core/Contracts/Context"
@@ -44,7 +45,13 @@ export default class MessageTransformer {
         (message.channel as GuildChannel).overwritePermissions(
           message.guild.roles.find(({ name }) => name === roleName),
           permissions
-        )
+        ),
+      getChannelMessages: (options?: ChannelLogsQueryOptions) =>
+        message.channel.fetchMessages(options),
+      deleteChannelMessages: (options?: ChannelLogsQueryOptions) =>
+        message.channel
+          .fetchMessages(options)
+          .then((messages) => message.channel.bulkDelete(messages))
     }
   }
 }
