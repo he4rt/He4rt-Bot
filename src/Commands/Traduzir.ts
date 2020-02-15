afb629a593ac97379096daf4c46f0e2c5a9fae58
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-02-14 23:47:30 -0300

    feat(Commands): add !traduzir <lang> <message>

---
+++ src/Commands/Traduzir.ts
@@ -0,0 +1,28 @@
+import Command from "@core/Contracts/Command"
+import Context from "@core/Contracts/Context"
+import InvalidArgsException from "@core/Exceptions/InvalidArgs"
+import translate from "@vitalets/google-translate-api"
+
+export default class Traduzir extends Command {
+  public get description() {
+    return "Traduz uma mensagem."
+  }
+
+  public help(): string {
+    return ":x: Como usar: `!traduzir <language(exemplo: en)> <text>`"
+  }
+
+  public validate(args: string[]): void | never {
+    if (args.length < 2) {
+      throw new InvalidArgsException(this.help())
+    }
+  }
+
+  public async run({ args, send }: Context): Promise<void> {
+    const to = args.pop()
+
+    const { text } = await translate(args.join(" "), { to })
+
+    await send(text)
+  }
+}
