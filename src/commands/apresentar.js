553f0de0d7f273f859158b1f1a821b525ecf22c9
Gustavo <gugumlhlimaa@gmail.com>
2020-04-16 17:48:15 -0300

    Add validate dm

--- src/commands/apresentar.js
+++ src/commands/apresentar.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const categories = require('../userCategory');
 
 module.exports = {
@@ -5,6 +6,9 @@ module.exports = {
 		const presentedRole = client.guilds
 			.get(process.env.GUILD_ID)
 			.roles.find(role => role.name === '🎓 Apresentou');
+		if (message.channel.type === 'dm') {
+			throw new Error('direct');
+		}
 		if (message.member.roles.has(presentedRole.id)) {
 			throw new Error('registered');
 		}
@@ -16,6 +20,17 @@ module.exports = {
 		return message.author.send(
 			'``❗`` Este é o nosso sistema de apresentação.\n\nResponda as perguntas com sinceridade total por sua pessoa.\nPara cancelar o envio, apenas ignore.\n\n``❗`` Para continuar digite ``!CONTINUAR`` aqui neste chat.'
 		);
+	},
+	async fail(err, client, message) {
+		if (err.message === 'direct') {
+			const directEmbed = new Discord.RichEmbed()
+				.setTitle(
+					'``❌`` **Você não pode utilizar este comando aqui.**'
+				)
+				.setColor('#36393E');
+			return message.channel.send(directEmbed);
+		}
+		return null;
 	},
 
 	get command() {
