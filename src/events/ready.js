059b1851fef103b57dc2b879d3e25d527dac3f73
Gustavo <gugumlhlimaa@gmail.com>
2020-08-26 15:36:48 -0300

    fix timer message

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,26 +1,4 @@
-const Discord = require('discord.js');
-const { scheduleJob } = require('node-schedule');
-
 module.exports = async client => {
-	const guild = client.guilds.get(process.env.GUILD_ID);
-
-	guild.members
-		.filter(m => m.roles.find('id', '735623088414261268'))
-		.map(m =>
-			m.sendMessage(
-				'Não esqueça de marcar seu 1 on 1 através do link https://calendly.com/he4rtmarketing/1on1 !'
-			)
-		);
-	scheduleJob('0 0 11 */15 * *', () => {
-		guild.members
-			.filter(m => m.roles.find('id', '735623088414261268'))
-			.map(m =>
-				m.sendMessage(
-					'Não esqueça de marcar seu 1 on 1 através do link https://calendly.com/he4rtmarketing/1on1 !'
-				)
-			);
-	});
-
 	client.user.setPresence({
 		status: 'online',
 		game: {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs-extra');
 const Enmap = require('enmap');
+const { scheduleJob } = require('node-schedule');
 
 const client = new Discord.Client({ forceFetchUsers: true });
 require('dotenv').config();
@@ -14,7 +15,7 @@ const init = async () => {
 		`Carregando o total de ${cmdFiles.length - 1} comandos.`
 	);
 	cmdFiles.shift();
-	cmdFiles.forEach((f) => {
+	cmdFiles.forEach(f => {
 		try {
 			// eslint-disable-next-line
 			const props = require(`./commands/${f}`);
@@ -30,7 +31,7 @@ const init = async () => {
 
 	const evtFiles = await fs.readdir('src/events/');
 	console.log('[#LOG]', `Carregando o total de ${evtFiles.length} eventos.`);
-	evtFiles.forEach((f) => {
+	evtFiles.forEach(f => {
 		const eventName = f.split('.')[0];
 		// eslint-disable-next-line
 		const event = require(`./events/${f}`);
@@ -38,9 +39,20 @@ const init = async () => {
 		client.on(eventName, event.bind(null, client));
 	});
 
-	client.on('error', (err) => console.error('[#ERROR]', err));
+	client.on('error', err => console.error('[#ERROR]', err));
 
 	client.login(process.env.AUTH_TOKEN);
+	const guild = client.guilds.get(process.env.GUILD_ID);
+
+	scheduleJob('0 0 11 */15 * *', () => {
+		guild.members
+			.filter(m => m.roles.find('id', '735623088414261268'))
+			.map(m =>
+				m.sendMessage(
+					'Não esqueça de marcar seu 1 on 1 através do link https://calendly.com/he4rtmarketing/1on1 !'
+				)
+			);
+	});
 };
 init();
 
