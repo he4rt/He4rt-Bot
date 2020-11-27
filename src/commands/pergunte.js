17141ec8b681e1762803558c18c1df499b7873f1
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-11-26 21:29:23 -0300

    feat(commands/pergunte): tag mentioned user if theres one

--- src/commands/pergunte.js
+++ src/commands/pergunte.js
@@ -2,15 +2,18 @@ const Discord = require('discord.js');
 const dedent = require('dedent');
 const categories = require('../userCategory');
 
+const linusSayingTalkIsCheap =
+	'https://recruitingdaily.com/wp-content/uploads/sites/6/2017/02/quote-talk-is-cheap-show-me-the-code-linus-torvalds-45-66-13-e1487242875427.jpg';
+
 module.exports = {
 	run(client, message) {
-		const linusSayingTalkIsCheap =
-			'https://recruitingdaily.com/wp-content/uploads/sites/6/2017/02/quote-talk-is-cheap-show-me-the-code-linus-torvalds-45-66-13-e1487242875427.jpg';
+		const mentionedUser = message.mentions.users.first();
 
 		const answer = new Discord.RichEmbed()
 			.setTitle('Não peça para perguntar')
 			.setDescription(
 				dedent`
+				${mentionedUser ? `Ei ${mentionedUser}` : ''}
 				Explique o problema
 				Mostre o que você tentou
 				Mostre o que deu errado
