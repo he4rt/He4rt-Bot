e381da4ef188c0299da8399f76ab3cb68cf27af5
Helen Dias <helendias.hd@gmail.com>
2021-09-23 21:29:34 -0300

    feat(juntos-bot): add initial message in chat and DM

--- src/commands/juntos.js
+++ src/commands/juntos.js
@@ -1,26 +1,52 @@
+const { promisify } = require('util');
 const Discord = require('discord.js');
 const categories = require('../userCategory');
 const util = require('../util');
 
+const sendInitialMessage = message =>
+	message.author.send(
+		new Discord.RichEmbed()
+			.setTitle('Processo seletivo da Juntos Somos Mais')
+			.setDescription(
+				`Vamos fazer algumas perguntas para te conhecer melhor.
+        São 9 perguntinhas, sendo apenas uma dissertativa, é rapidinho!
+
+        Todas as informações desse questionário serão enviadas ao time de People da Juntos Somos Mais, que poderá entrar em contato contigo através de DM no Discord durante a He4rt Conf ${util.getYear()} ou DM no LinkedIn após o evento.
+
+        Para cancelar o envio, apenas ignore.`
+			)
+			.setFooter(
+				`${util.getYear()} © He4rt Developers`,
+				'https://i.imgur.com/14yqEKn.png'
+			)
+			.setTimestamp()
+	);
+
+const sendChannelMessage = message =>
+	message.channel.send(
+		new Discord.RichEmbed()
+			.setTitle('Processo seletivo da Juntos Somos Mais')
+			.setDescription(
+				`Olá, ${message.author.username}!
+          Agradecemos o interesse em participar do processo seletivo da Juntos Somos Mais na He4rt Conf 2021!
+
+          Para continuar, te enviamos um questionário via DM no Discord.
+          Por favor, responda o questionário para prosseguirmos!
+          
+          Até logo o/
+        `
+			)
+			.setFooter(
+				`${util.getYear()} © He4rt Developers`,
+				'https://i.imgur.com/14yqEKn.png'
+			)
+			.setTimestamp()
+	);
+
 module.exports = {
 	async run(client, message) {
-		return message.channel.send(
-			new Discord.RichEmbed()
-				.setTitle('Processo seletivo da Juntos Somos Mais')
-				.setDescription(
-					`Vamos fazer algumas perguntas para te conhecer melhor.
-          São 9 perguntinhas, sendo apenas uma dissertativa, é rapidinho!
-
-          Todas as informações desse questionário serão enviadas ao time de People da Juntos Somos Mais, que poderá entrar em contato contigo através de DM no Discord durante a He4rt Conf ${util.getYear()} ou DM no LinkedIn após o evento.
-
-          Para cancelar o envio, apenas ignore.`
-				)
-				.setFooter(
-					`${util.getYear()} © He4rt Developers`,
-					'https://i.imgur.com/14yqEKn.png'
-				)
-				.setTimestamp()
-		);
+		sendChannelMessage(message);
+		await sendInitialMessage(message);
 	},
 
 	get command() {
